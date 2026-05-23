8399d01ae8aff7395b154f5ac08dc382c78100a3
rohit-pandey45 <rpandey1627@gmail.com>
2026-05-23 13:36:32 +0530

    CLEANING THE CODE

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Rohit's React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <h1> Rohit's React with vite</h1>
+  )
+}
+
+export default App
