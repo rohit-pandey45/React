84187306d095cd9e02d6e9879356b3e1c90d6965
rohit-pandey45 <rpandey1627@gmail.com>
2026-05-27 02:52:47 +0530

    Responsive

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -38,8 +38,8 @@ function App() {
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg
       px-4 py-3 my-8 bg-gray-800 text-orange-500">
-        <h1 className='text-white text-center my-3'>Password Generator</h1>
-      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <h1 className='text-white text-3xl text-center my-3 font-bold leading-normal'>Password Generator</h1>
+      <div className="flex flex-col sm:flex-row shadow rounded-lg overflow-hidden mb-4 gap-2">
         <input 
           type="text" 
           value={password}
@@ -53,7 +53,7 @@ function App() {
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
       </div>
-      <div className='flex text-sm gap-x-2'>
+      <div className='flex flex-wrap text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
           <input 
             type="range"
